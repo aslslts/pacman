cbbe695b374290d53b9ceaa4b6b38dd69b9d4ff9
Aslı Sulutaş <cemree.1402@gmail.com>
2022-11-05 21:03:14 +0300

    Pacman class added

---
+++ index.js
@@ -0,0 +1,50 @@
+import { LEVEL, OBJECT_TYPE } from "./setup";
+
+//Classes
+import GameBoard from "./GameBoard";
+import Pacman from "./Pacman";
+
+// DOM Elements
+
+const gameGrid = document.querySelector("#game");
+const scoreTable = document.querySelector("#score");
+const startButton = document.querySelector("#start-button");
+
+// Game Constants
+
+const POWER_PILL_TIME = 10000; //ms
+const GLOBAL_SPEED = 80; //ms
+const gameBoard = GameBoard.createGameBoard(gameGrid, LEVEL);
+
+//Initial Setup
+
+let score = 0;
+let timer = null;
+let gameWin = false;
+let powerPillActive = false;
+let powerPillTimer = null;
+
+function gameOver(pacman, grid) {}
+
+function checkCollision(pacman, ghosts) {}
+
+function gameLoop(pacman, ghosts) {}
+
+function startGame() {
+  gameWin = false;
+  powerPillActive = false;
+  score = 0;
+
+  startButton.classList.add("hide");
+
+  gameBoard.createGrid(LEVEL);
+
+  const pacman = new Pacman(2, 287);
+  gameBoard.addObject(287, [OBJECT_TYPE.PACMAN]);
+  document.addEventListener("keydown", (e) =>
+    pacman.handleKeyInput(e, gameBoard.objectExist)
+  );
+}
+
+// Initialize game
+startButton.addEventListener("click", startGame);
